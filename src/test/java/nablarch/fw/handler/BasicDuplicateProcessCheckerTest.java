0ee8784aee003540d5bf1375c689f4dc75c8a7f3
opengl-8080 <opengl-8080@users.noreply.github.com>
2023-02-21 15:12:50 +0900

    Jakarta EE 対応 (#13)
    * javax -> jakarta
    
    * fix: Java 8 以上でキャストエラーになる問題を修正
    
    ---------
    
    Co-authored-by: yamamoto-tis <73279508+yamamoto-tis@users.noreply.github.com>

--- src/test/java/nablarch/fw/handler/BasicDuplicateProcessCheckerTest.java
+++ src/test/java/nablarch/fw/handler/BasicDuplicateProcessCheckerTest.java
@@ -3,10 +3,10 @@ package nablarch.fw.handler;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import nablarch.core.db.transaction.SimpleDbTransactionManager;
 import nablarch.core.repository.SystemRepository;
